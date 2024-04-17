d334c9d32be26cc21c4bfdf689e79d33cebc752c
Manuel <manuelfabrizzio123@gmail.com>
2024-04-16 21:54:15 -0500

    prueba

---
+++ animal.js
@@ -0,0 +1,10 @@
+function pregunta(){
+    let respuesta = prompt("Piero es huevon?");
+    if(respuesta == "si"){
+        alert("Animal");
+    }
+    else if ( respuesta == "no"){
+        alert("Burro burro burro");
+    }
+}
+pregunta();
